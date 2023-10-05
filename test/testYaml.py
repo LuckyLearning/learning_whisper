4f56802f9182f76a27014095eac0808cd1c31d81
LuckyLearning <lf19950801@gmail.com>
2023-10-05 05:34:13 +0000

    add tip

--- test/testYaml.py
+++ test/testYaml.py
@@ -2,9 +2,10 @@ import os
 
 import yaml
 
-filepath = "../config.yml"
+filepath = "config.yml"
 
 if not os.path.exists(filepath):
+    print("#"*20)
     print("文件不存在")
 
 with open(filepath, encoding="utf8") as f:
